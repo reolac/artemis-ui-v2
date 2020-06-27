883e30044acb440306dc5e15af53c48450e05636
Michael Smith <reolac@gmail.com>
2020-06-27 17:55:34 +0100

    Updates

---
+++ precache-manifest.137074ac96795b1dcc2b83c08f1fe31b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7d489b9db91c83216a40fc765d7b85",
+    "url": "/artemis-ui-v2/index.html"
+  },
+  {
+    "revision": "9f56b0bba1649f2e5d60",
+    "url": "/artemis-ui-v2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7a3553e5dceea5e415c0",
+    "url": "/artemis-ui-v2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f56b0bba1649f2e5d60",
+    "url": "/artemis-ui-v2/static/js/2.dde45297.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/artemis-ui-v2/static/js/2.dde45297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3553e5dceea5e415c0",
+    "url": "/artemis-ui-v2/static/js/main.cf0e7a79.chunk.js"
+  },
+  {
+    "revision": "e6b9ae850bd9d9d06eba",
+    "url": "/artemis-ui-v2/static/js/runtime-main.2a1d8c05.js"
+  }
+]);
